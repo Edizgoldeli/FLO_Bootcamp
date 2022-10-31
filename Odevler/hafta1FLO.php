99d1ae159db7bf1adfdf320bb7c1506fe77c4603
Ediz <edizgoldeli@hotmail.com>
2022-10-31 22:39:25 +0300

    deleted empty lines

--- Odevler/hafta1FLO.php
+++ Odevler/hafta1FLO.php
@@ -10,33 +10,29 @@ Eğer boş kalan ağıllar varsa 0 (sıfır) değerle ekrana yazdır.
 $barnCount = 5;
 $totalCapacity = 150;
 $sheepCount = 161;
-$capacity = $totalCapacity / $barnCount;
-$x = array();
+$capacity = $totalCapacity / $barnCount; //capacity of each barn
 $y = array();
-echo "Single Barn capacity: $capacity <br> Total Capacity: $totalCapacity <br> Sheep Count: $sheepCount <br><br>";
+echo "Single Barn capacity: $capacity <br> Total Capacity: $totalCapacity <br> Sheep Count: $sheepCount <br><br>"; // values of variables
 for ($i = $barnCount; $i > 0; $i--) {
-
-    array_push($x, "Barn$i");
-    array_push($y, "$capacity");
-    
+    array_push($y, "$capacity"); // declared capacity of each barn with order
 }
-$barnCountRemain = $barnCount;
-foreach ($y as $space) {
-    
-    if ($sheepCount >= $space) {
-        $sheepCount = $sheepCount - $capacity;
-        $space = $capacity-$space;
-    } else if ($sheepCount < $space) {
-$space = $space - $sheepCount;
+$barnCountRemain = $barnCount; // declared this variable to display name of the barns
+foreach ($y as $space) { //used foreach to work on all declared spaces with order
 
+    if ($sheepCount >= $space) { //if we have space lower than sheeps this will work
+        $sheepCount = $sheepCount - $capacity; //reduced the filled amount of the sheeps from total
+        $space = $capacity - $space; //redeclared the space after we filled with sheeps
+    } else if ($sheepCount < $space) { // this will work if we have more space than sheeps
+        $space = $space - $sheepCount; // since we have more space we will avoid getting negative result
     }
+    $remaininCapacity = $capacity - $space; // it showes filled space in each barn
 
-   // $totalCapacity = $totalCapacity - $sheepCount;
-$remaininCapacity = $capacity-$space;
-
-    echo "Barn $barnCountRemain remains: " . $remaininCapacity . "<br>";
-    $barnCountRemain = $barnCountRemain-1;
+    echo "Barn $barnCountRemain remains: " . $remaininCapacity . "<br>"; //displaying barn number and filled space
+    $barnCountRemain = $barnCountRemain - 1; //reducing the variable by 1 to not show same name on each barn
 }
-if($space==0){ echo "<br>Sheeps remain leftout: " . ($sheepCount) . "<br>";}
-   
-
+if ($space == 0) { //works if space equals to 0
+    echo "<br>Sheeps remain leftout: " . ($sheepCount) . "<br>"; //Shows the sheeps that leftout.
+} else if ($space > 0) {} //we avoid to give errors for true statements
+else {
+    echo "ERROR!"; //user will get error if there is negative space
+} 
