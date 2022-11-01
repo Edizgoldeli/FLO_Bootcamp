3c1326f063513bdbeb8204c8b7f1b52212409941
Ediz <edizgoldeli@hotmail.com>
2022-11-01 16:12:30 +0300

    *

--- sessions.php
+++ sessions.php
@@ -1,1 +1,17 @@
 <?php
+session_start();
+
+$_SESSION["kitap"] = "Atatürk";
+$_SESSION["yil"] = 2020;
+
+echo "Yazar". $_SESSION["kitap"]."<br> yıl: ". $_SESSION["yil"];
+
+if (isset($_SESSION["kitap"])){
+    echo "oturuma hoşgeldiniz"
+}
+if( empty($_SESSION["yil"])){
+    //içerik boşsa xxx
+}else{//içerik doluydu xxx
+}
+unset($_SESSION["kitap"],$_SESSION["yil"]);
+session_destroy();//tüm sessionlar kapatılır
