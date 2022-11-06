6cb2d01ff94e77d7fe24fbe482aa46aee0a54e34
Ediz <edizgoldeli@hotmail.com>
2022-11-06 18:38:19 +0300

    .

---
+++ Uygulamalı/uygulama1.php
@@ -0,0 +1,13 @@
+<?php
+<form method="post" action="kontrol.php">
+<b>Adı Soyadı:</b><br>
+<input type="text" name="adsoyad">
+<br><br>
+<b>E-mail Adresi:</b><br>
+<input type="email" name="email">
+<br><br>
+<b>Telefon Numarası:</b><br>
+<input type="tel" name="telefon">
+<br><br>
+<input type="submit" value="Kaydet">
+</form>
