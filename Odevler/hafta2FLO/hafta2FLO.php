1b2f6108da3b2e9d2928108c6fcbc24d4f0f50ec
Ediz <edizgoldeli@hotmail.com>
2022-11-09 13:00:36 +0300

    Ödev 2

--- Odevler/hafta2FLO/hafta2FLO.php
+++ Odevler/hafta2FLO/hafta2FLO.php
@@ -1,38 +1,38 @@
 <?php
 
 session_start();
-$urun1 = "Ülker Çikolatalı Gofret 40 gr";
-$urun1Fiyat = "10₺";
+$urun1 = "Ülker Çikolatalı Gofret 40 gr"; //ürün ismini tanımladık
+$urun1Fiyat = "10₺"; // ürün fiyatını tanımladık
 $urun2 = "Eti Damak Kare Çikolata 60 gr";
 $urun2Fiyat = "20₺";
 $urun3 = "Nestle Bitter Çikolata 50 gr";
 $urun3Fiyat = "20₺";
-$adet1 = $_SESSION["adet1"];
+$adet1 = $_SESSION["adet1"]; //daha önceki işlemlerden gelen ürün bilgisi için session kullandık
 $adet2 = $_SESSION["adet2"];
 $adet3 = $_SESSION["adet3"];
-$fiyat1 = $adet1 * $urun1Fiyat;
+$fiyat1 = $adet1 * $urun1Fiyat; //birden fazla eklenen ürünlerin fiyatını hesapladık
 $fiyat2 = $adet2 * $urun2Fiyat;
 $fiyat3 = $adet3 * $urun3Fiyat;
-$total = $fiyat1 + $fiyat2 + $fiyat3;
+$total = $fiyat1 + $fiyat2 + $fiyat3; //sepet tutarını hesapladık
 ?>
 <!DOCTYPE html>
 
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel=stylesheet href="style.css" />
-    <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet'>
-    <title> Odev 2 </title>
+    <meta charset="utf-8"> <!-- karakter tipini belirledik -->
+    <meta name="viewport" content="width=device-width, initial-scale=1" /> <!-- mobil cihazlar için ölçek ayarı yaptık -->
+    <link rel=stylesheet href="style.css" /> <!-- style.css dosyasını ekledik -->
+    <link href='https://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet'> <!-- Quicksand font'unun kaynağını ekledik -->
+    <title> Odev 2 </title> <!-- başlığı belirledik -->
 </head>
 
 <body>
     <center>
         <br>
-        <table border='1' width='95%'>
+        <table border='1' width='95%'> <!-- tablo oluşturup genişliği sayfanın %95'i olacak şekilde ayarladık -->
             <tr>
-                <td><b>Ürün Adı</b></td>
+                <td><b>Ürün Adı</b></td> <!-- tablo başlıklarını ayarladık -->
                 <td class="cntr"><b>Ürün Fiyatı</b></td>
                 <td class="cntr"><b>Adet</b></td>
             </tr>
@@ -40,25 +40,25 @@ $total = $fiyat1 + $fiyat2 + $fiyat3;
 
 
             <tr>
-                <form method="post" action="sepetKontrol.php">
+                <form method="post" action="sepetKontrol.php"> <!-- form işlemini post metoduyla sepetKontrol.php'ye yollayacağımızı belirledik -->
                     <td><?php echo $urun1; ?></td>
-                    <td class="cntr"><?php echo $urun1Fiyat; ?></td>
-                    <td class="cntr"><input type="number" name="adet1" min="1" max="50" placeholder="0"></td>
+                    <td class="cntr"><?php echo $urun1Fiyat; ?></td> 
+                    <td class="cntr"><input type="number" name="adet1" min="0" max="50" placeholder="0"></td> <!-- ürün adedi eklenebilmesi için number türünde girdi alanı ekledik ve min max ile aralığı daraltıp kabul edilmeyen girdilerin engellenmesini sağladık ( php tarafında bir kontrol daha var mininimum için ) -->
             </tr>
             <tr>
                 <td><?php echo $urun2; ?></td>
                 <td class="cntr"><?php echo $urun2Fiyat; ?></td>
-                <td class="cntr"><input type="number" name="adet2" min="1" max="50" placeholder="0"></td>
+                <td class="cntr"><input type="number" name="adet2" min="0" max="50" placeholder="0"></td>
             </tr>
             <tr>
                 <td><?php echo $urun3; ?></td>
                 <td class="cntr"><?php echo $urun3Fiyat; ?></td>
-                <td class="cntr"><input type="number" name="adet3" min="1" max="50" placeholder="0"></td>
+                <td class="cntr"><input type="number" name="adet3" min="0" max="50" placeholder="0"></td>
             </tr>
 
         </table>
 
-        <input type="submit" class="submit" value="Ürünü sepete ekle">
+        <input type="submit" class="submit" value="Ürünü sepete ekle"> 
         </form>
 
         <table border='1' width='95%'>
@@ -91,7 +91,7 @@ $total = $fiyat1 + $fiyat2 + $fiyat3;
             </tr>
         </table>
         <form method="post" action="sepetKontrol.php">
-            <input type="hidden" name="temizle" value="1">
+            <input type="hidden" name="temizle" value="1"> <!-- sepeti temizlemek için php tarafında tüm seçili ürünleri silen fonksiyonu çalıştırmak için 1 değerini yolladık -->
             <input type="submit" class="submit" value="Sepeti Temizle">
     </center>
 </body>
