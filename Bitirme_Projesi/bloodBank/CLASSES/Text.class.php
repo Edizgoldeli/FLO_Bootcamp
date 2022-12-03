8b34c52dbc5868b91f59e90bfb1159c2a887c782
Ediz Göldeli <38820143+Edizgoldeli@users.noreply.github.com>
2022-12-03 09:48:18 +0300

    commit

--- Bitirme_Projesi/bloodBank/CLASSES/Text.class.php
+++ Bitirme_Projesi/bloodBank/CLASSES/Text.class.php
@@ -8,4 +8,12 @@ class Text
         $number = number_format($number, 2, ',', '');
         return $number;
     }
+    public function upper($text)
+    {
+        $search = array("ç", "i", "ı", "ğ", "ö", "ş", "ü");
+        $replace = array("Ç", "İ", "I", "Ğ", "Ö", "Ş", "Ü");
+        $text = str_replace($search, $replace, $text);
+        $text = strtoupper($text);
+        return $text;
+    }
 }
